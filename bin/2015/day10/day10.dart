e4c9b9aae9f6785ed6e5b656db12029e27105eff
Sébastien Cottinet <sebastien@coredumped.io>
2024-03-08 22:56:02 +0100

    refactor: private variables

--- bin/2015/day10/day10.dart
+++ bin/2015/day10/day10.dart
@@ -3,18 +3,18 @@ import 'dart:async';
 import 'package:aoc2015/runnable.dart';
 
 class Y2015Day10 extends Runnable {
-  final groupRxp = RegExp(r'(.)\1*');
-  String content = "";
+  final _groupRxp = RegExp(r'(.)\1*');
+  String _content = "";
 
   @override
   FutureOr<void> init(Stream<String> input,
       {List<String> args = const []}) async {
-    content = await input.join();
+    _content = await input.join();
   }
 
   String lookAndSay(String buf) {
     String result = "";
-    final matches = groupRxp.allMatches(buf);
+    final matches = _groupRxp.allMatches(buf);
 
     result = matches
         .map((match) => "${match.end - match.start}${buf[match.start]}")
@@ -25,7 +25,7 @@ class Y2015Day10 extends Runnable {
 
   @override
   void part1() {
-    String buf = content;
+    String buf = _content;
 
     for (int i = 0; i < 40; i++) {
       buf = lookAndSay(buf);
@@ -36,7 +36,7 @@ class Y2015Day10 extends Runnable {
 
   @override
   void part2() {
-    String buf = content;
+    String buf = _content;
 
     for (int i = 0; i < 50; i++) {
       buf = lookAndSay(buf);
