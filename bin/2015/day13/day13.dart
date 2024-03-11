c9351e5aad4d63a99a2109ed1d7f55dc777d97c4
Sébastien Cottinet <sebastien@coredumped.io>
2024-03-11 14:19:40 +0100

    feat: year 2015, day 13, part 2

--- bin/2015/day13/day13.dart
+++ bin/2015/day13/day13.dart
@@ -8,6 +8,7 @@ import 'package:aoc2015/runnable.dart';
 
 class Y2015Day13 implements Runnable {
   final Map<String, WeightedGraphNode<String>> nodes = {};
+  double withoutMeCost = 0;
 
   @override
   FutureOr<void> init(Stream<String> input,
@@ -30,10 +31,11 @@ class Y2015Day13 implements Runnable {
     }
   }
 
-  @override
-  FutureOr<void> part1() {
-    // Priciest route: we need to invert the sign of weights.
-    // Travelling Salesman: all edge weights must be symetrical
+  WeightedGraphNode<String> prepareForTSP() {
+    // TSP finds the cheapiest route, we want the costliest one: we need to
+    // invert the sign of weights.
+    //
+    // Also, TSP requires that all edge weights are symetrical
     final copy = nodes.values.first.deepCopy();
     final seen = <WeightedGraphNode<String>>{};
     final queue = Queue<WeightedGraphNode<String>>.from([copy]);
@@ -54,6 +56,12 @@ class Y2015Day13 implements Runnable {
       }
     }
 
+    return copy;
+  }
+
+  @override
+  FutureOr<void> part1() {
+    final copy = prepareForTSP();
     final path = travellingSalesman(copy);
 
     for (int i = 0; i < path.costs.length; i++) {
@@ -61,10 +69,43 @@ class Y2015Day13 implements Runnable {
           "${path.path[i].data} -> ${path.path[i + 1].data} = ${-path.costs[i]}");
     }
 
-    print(
-        "Cheapiest path: ${path.costs.fold<double>(0, (agg, val) => (-1.0 * val + agg))}");
+    withoutMeCost =
+        path.costs.fold<double>(0, (agg, val) => (-1.0 * val + agg));
+
+    print("Cheapiest path: $withoutMeCost");
   }
 
   @override
-  FutureOr<void> part2() {}
+  FutureOr<void> part2() {
+    final copy = prepareForTSP();
+    final me = WeightedGraphNode(data: "me");
+    final queue = Queue<WeightedGraphNode<String>>.from([copy]);
+
+    while (queue.isNotEmpty) {
+      final node = queue.removeFirst();
+      if (me.children.containsKey(node)) continue;
+
+      for (final c in node.children.keys) {
+        if (me.children.containsKey(c)) continue;
+
+        queue.add(c);
+        me.children[c] = 0;
+        c.children[me] = 0;
+      }
+
+      me.children[node] = 0;
+      node.children[me] = 0;
+    }
+
+    final path = travellingSalesman(copy);
+
+    for (int i = 0; i < path.costs.length; i++) {
+      print(
+          "${path.path[i].data} -> ${path.path[i + 1].data} = ${-path.costs[i]}");
+    }
+
+    double cost = path.costs.fold<double>(0, (agg, val) => (-1.0 * val + agg));
+
+    print("Cheapiest path: $cost (diff with part 1: ${withoutMeCost - cost})");
+  }
 }
