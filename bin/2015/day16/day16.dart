968ce310e58482cb6cf7287db709e6f9fd93aec1
Sébastien Cottinet <sebastien@coredumped.io>
2024-04-07 22:27:39 +0200

    feat: year 2015, day 16, part 2

--- bin/2015/day16/day16.dart
+++ bin/2015/day16/day16.dart
@@ -5,6 +5,18 @@ import 'aunt_sue.dart';
 
 class Y2015Day16 implements Runnable {
   final _aunts = <AuntSue>[];
+  final mfcsamOutput = <String, int>{
+    "children": 3,
+    "cats": 7,
+    "samoyeds": 2,
+    "pomeranians": 3,
+    "akitas": 0,
+    "vizslas": 0,
+    "goldfish": 5,
+    "trees": 3,
+    "cars": 2,
+    "perfumes": 1
+  };
 
   @override
   FutureOr<void> init(Stream<String> input,
@@ -16,25 +28,39 @@ class Y2015Day16 implements Runnable {
 
   @override
   FutureOr<void> part1() {
-    final searched = <String, int>{
-      "children": 3,
-      "cats": 7,
-      "samoyeds": 2,
-      "pomeranians": 3,
-      "akitas": 0,
-      "vizslas": 0,
-      "goldfish": 5,
-      "trees": 3,
-      "cars": 2,
-      "perfumes": 1
-    };
-
     final found = _aunts.firstWhere((aunt) => aunt.compounds.entries
-        .every((entry) => searched[entry.key] == entry.value));
+        .every((entry) => mfcsamOutput[entry.key] == entry.value));
 
     print('Found aunt: ${found.number}');
   }
 
   @override
-  FutureOr<void> part2() {}
+  FutureOr<void> part2() {
+    for (final aunt in _aunts) {
+      bool matched = true;
+
+      for (final MapEntry(key: compound, value: qty) in mfcsamOutput.entries) {
+        final searchedQty = aunt.compounds[compound];
+
+        switch (compound) {
+          case 'cats':
+          case 'trees':
+            matched = searchedQty == null || searchedQty > qty;
+            break;
+          case 'pomeranians':
+          case 'goldfish':
+            matched = searchedQty == null || searchedQty < qty;
+            break;
+          default:
+            matched = searchedQty == null || searchedQty == qty;
+        }
+
+        if (!matched) break;
+      }
+
+      if (matched) {
+        print('Found aunt: ${aunt.number}');
+      }
+    }
+  }
 }
