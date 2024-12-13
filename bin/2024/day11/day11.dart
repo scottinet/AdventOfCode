fe02f2f29a162e426f1924f0429493946f4311ef
Sébastien Cottinet <sebastien@coredumped.io>
2024-12-13 11:32:54 +0100

    feat: year 2024, day 11, part 02

--- bin/2024/day11/day11.dart
+++ bin/2024/day11/day11.dart
@@ -43,6 +43,88 @@ final class Y2024Day11 extends Runnable {
     print("There are now ${stones.length} stones");
   }
 
+  ExpandResult expand(List<int> stones, int times) {
+    List<(List<int> stones, int i)> remaining = [];
+
+    for (int i = 0; i < times && stones.isNotEmpty; i++) {
+      final List<int> next = [];
+
+      for (int s = 0; s < stones.length; s++) {
+        final str = stones[s].toString();
+
+        if (stones[s] == 0) {
+          next.add(1);
+        } else if (str.length % 2 == 0) {
+          final pivot = str.length ~/ 2;
+          next.add(int.parse(str.substring(0, pivot)));
+          next.add(int.parse(str.substring(pivot)));
+        } else {
+          next.add(stones[s] * 2024);
+        }
+      }
+
+      stones = [];
+      final List<int> setAside = [];
+      for (final s in next) {
+        if (s.toString().length > 1) {
+          stones.add(s);
+        } else {
+          setAside.add(s);
+        }
+      }
+
+      if (setAside.isNotEmpty) {
+        remaining.add((setAside, i + 1));
+      }
+    }
+
+    return (count: stones.length, remaining: remaining);
+  }
+
+  int expandSingle(int stone, int remainingSteps, Map<(int, int), int> cache) {
+    final known = cache[(stone, remainingSteps)];
+
+    if (known != null) return known;
+
+    if (remainingSteps > 0) {
+      var (:count, :remaining) = expand([stone], remainingSteps);
+
+      for (final r in remaining) {
+        final todo = remainingSteps - r.$2;
+
+        if (todo == 0) {
+          count += r.$1.length;
+        } else {
+          for (final l in r.$1) {
+            count += expandSingle(l, todo, cache);
+          }
+        }
+      }
+
+      cache[(stone, remainingSteps)] = count;
+      return count;
+    }
+
+    return 1;
+  }
+
   @override
-  void part2() {}
+  void part2() {
+    const blinks = 75;
+
+    var (:count, :remaining) = expand(_stones, blinks);
+    Map<(int, int), int> cache = {};
+
+    while (remaining.isNotEmpty) {
+      final (stones, i) = remaining.removeLast();
+
+      for (final s in stones) {
+        count += expandSingle(s, blinks - i, cache);
+      }
+    }
+
+    print("There are now $count stones");
+  }
 }
+
+typedef ExpandResult = ({int count, List<(List<int> stones, int i)> remaining});
