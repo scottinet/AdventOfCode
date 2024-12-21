43cdd45ec0484237f989cf6a749aad3c16677874
Sébastien Cottinet <sebastien@coredumped.io>
2024-12-21 15:58:25 +0100

    feat: year 2024, day 21, part 02

--- bin/2024/day21/day21.dart
+++ bin/2024/day21/day21.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:advent_of_code/runnable.dart';
 
 final class Y2024Day21 extends Runnable {
-  static const Map<String, (int, int)> _numKeypad = {
+  static const Map<String, (int, int)> _numPad = {
     "7": (0, 0),
     "8": (1, 0),
     "9": (2, 0),
@@ -18,7 +18,7 @@ final class Y2024Day21 extends Runnable {
     "0": (1, 3),
     "A": (2, 3)
   };
-  static const Map<String, (int, int)> _digitalKeypad = {
+  static const Map<String, (int, int)> _dPad = {
     "X": (0, 0),
     "^": (1, 0),
     "A": (2, 0),
@@ -36,62 +36,67 @@ final class Y2024Day21 extends Runnable {
     _codes.addAll(lines);
   }
 
-  String getPadPath(Map<String, (int, int)> pad, String from, String to) {
-    bool toReverse = false;
-    final spos = pad[from]!, epos = pad[to]!, forbidden = pad['X'];
-    (int, int) pos = spos;
-    (int, int) diff = (epos.$1 - spos.$1, epos.$2 - spos.$2);
-    final List<String> path = [];
+  String getPath(Map<String, (int, int)> pad, String from, String to) {
+    final spos = pad[from]!, epos = pad[to]!, forbidden = pad['X']!;
+    final dx = epos.$1 - spos.$1, dy = epos.$2 - spos.$2;
+    final h = (dx < 0 ? "<" : ">") * dx.abs();
+    final v = (dy < 0 ? "^" : "v") * dy.abs();
 
-    while (diff != (0, 0)) {
-      if (diff.$1 != 0) {
-        final toApply = diff.$1 ~/ diff.$1.abs();
-        diff = (diff.$1 - toApply, diff.$2);
-        pos = (pos.$1 + toApply, pos.$2);
-        path.add(toApply < 0 ? "<" : ">");
-      } else {
-        final toApply = diff.$2 ~/ diff.$2.abs();
-        path.add(toApply < 0 ? "^" : "v");
-        diff = (diff.$1, diff.$2 - toApply);
-        pos = (pos.$1, toApply + pos.$2);
-      }
-
-      toReverse = toReverse || pos == forbidden;
+    if (dx < 0 && (epos.$1, spos.$2) != forbidden ||
+        (spos.$1, epos.$2) == forbidden) {
+      return "$h${v}A";
     }
 
-    return (toReverse ? path.reversed : path).join("");
+    return "$v${h}A";
+  }
+
+  int getLength(Map<(String, int), int> cache, String seq, int depth) {
+    if (cache.containsKey((seq, depth))) {
+      return cache[(seq, depth)]!;
+    }
+
+    if (depth == 0) return seq.length;
+
+    int count = 0;
+
+    String prev = "A";
+    for (int i = 0; i < seq.length; i++) {
+      count += getLength(cache, getPath(_dPad, prev, seq[i]), depth - 1);
+      prev = seq[i];
+    }
+
+    cache[(seq, depth)] = count;
+    return count;
+  }
+
+  int getComplexity(int robots) {
+    int complexity = 0;
+    final Map<(String, int), int> cache = {};
+
+    for (final code in _codes) {
+      int numPart = int.parse(code.replaceAll("A", ""));
+      String movements = "";
+      String prev = "A";
+
+      for (final digit in code.split("")) {
+        movements += getPath(_numPad, prev, digit);
+        prev = digit;
+      }
+
+      final count = getLength(cache, movements, robots);
+      complexity += numPart * count;
+    }
+
+    return complexity;
   }
 
   @override
   void part1() {
-    int complexity = 0;
-
-    for (final code in _codes) {
-      int numPart = int.parse(code.replaceAll("A", ""));
-      List<String> movements = [];
-      String prev = "A";
-
-      for (final digit in code.split("")) {
-        movements.add("${getPadPath(_numKeypad, prev, digit)}A");
-        prev = digit;
-      }
-
-      for (int i = 0; i < 2; i++) {
-        final sequence = movements.join("");
-        prev = "A";
-        movements.clear();
-
-        for (int j = 0; j < sequence.length; j++) {
-          movements.add("${getPadPath(_digitalKeypad, prev, sequence[j])}A");
-          prev = sequence[j];
-        }
-      }
-      complexity += numPart * movements.fold(0, (agg, val) => agg + val.length);
-    }
-
-    print("Total complexity: $complexity");
+    print("Total complexity: ${getComplexity(2)}");
   }
 
   @override
-  void part2() {}
+  void part2() {
+    print("Total complexity: ${getComplexity(25)}");
+  }
 }
